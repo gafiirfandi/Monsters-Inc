929de00d9b29a3863aaf67c1534f112ef34abb09
gafiirfandi <gafiirfandi45@gmail.com>
2020-09-23 22:29:50 +0700

    Updates

---
+++ precache-manifest.80aff412b4d4923107aa37c5330a7314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6387d7da025f31a040dc457c43feb3",
+    "url": "/Monsters-Inc/index.html"
+  },
+  {
+    "revision": "1e89795103a0b0c28dcb",
+    "url": "/Monsters-Inc/static/css/main.55c8db06.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/Monsters-Inc/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Inc/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e89795103a0b0c28dcb",
+    "url": "/Monsters-Inc/static/js/main.982529c2.chunk.js"
+  },
+  {
+    "revision": "3dce6bcbaa4ade498c1e",
+    "url": "/Monsters-Inc/static/js/runtime-main.2d71868f.js"
+  }
+]);
